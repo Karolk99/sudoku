d33b47e37a4b02ffc64ccecea68181f83c4445a8
Karolk99 <konkolisz@gmail.com>
2021-04-08 13:26:47 +0200

    Add puzzles from file

--- sudoku.py
+++ sudoku.py
@@ -40,9 +40,12 @@ class Sudoku:
             except:
                 print("Oops!", sys.exc_info()[0], "occurred. Try again")
 
-    def choose_static_fields(self, fields : list) -> None:
+    def choose_static_fields(self, fields : list=None, path : str=None) -> None:
         self.array = np.zeros((self.size, self.size))
         self.static_fields = {}
+
+        if path != None:
+            fields = self.read_puzzle_from_file(path)
 
         for count, field in enumerate(fields):  # field is a tuple looking like this: (x, y, value)
             try:
@@ -53,7 +56,7 @@ class Sudoku:
                 print(f"In {count} element")
 
     def fill_in_array(self) -> None:
-         if self.array is None:
+        if self.array is None:
             self.array = np.zeros((self.size, self.size))
 
         numbers = [x for x in range(1, self.size + 1) for _ in range(1, self.size + 1)]
@@ -68,6 +71,16 @@ class Sudoku:
                     numbers.remove(value)
                     row[i] = value
 
+    def read_puzzle_from_file(self, path : str) -> list:
+        fields = open(path).readlines()
+        
+        for i in range(len(fields)):
+            fields[i] = fields[i].split(',')
+            fields[i][2] = fields[i][2][:-1]
+            fields[i] = tuple(fields[i])
+        
+        return fields
+            
     def __shufle(self):
         pass                                #do ustalenia, np mozemy narazie zalozyc, ze wszystkie pola sa ruchome to wtedy wydaje sie to byc prosty
                                        #do rozwiazania problem np tasujemy tak zeby zawsze dojsc do tego samego ulozenia (mamy jakis wzorzec ) ale to tylko sugestia :)
@@ -77,5 +90,4 @@ class Sudoku:
 
 if __name__ == '__main__':
     sudoku = Sudoku()
-    sudoku.create_sudoku_puzzle()
-    print(sudoku.array)
+    sudoku.read_puzzle_from_file("sudoku/puzzles/puzzle1.txt")
